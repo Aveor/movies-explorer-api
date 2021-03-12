8ab8cf7abce18264cdd0ae7ff0808b794e8ed61e
Anton Shubin <antonsh2005@yandex.ru>
2021-03-12 19:28:48 +0300

    добавbk куки и почистил

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,4 @@
 const Movie = require('../models/movie');
-// const ForbiddenError = require('../errors/ForbiddenError');
 const ValidationError = require('../errors/ValidationError');
 
 const getMovies = (req, res, next) => {
@@ -44,23 +43,6 @@ const addMovie = (req, res, next) => {
     })
     .catch((err) => next(err));
 };
-
-// const deleteMovie = (req, res, next) => {
-//   Movie.findById(req.params.movieId)
-//     .select('+owner')
-//     .then((movie) => {
-//       if (!movie) {
-//         throw new ValidationError('Ошибка валидации');
-//       }
-//       if (movie.owner.toString() !== req.user._id.toString()) {
-//         throw new ForbiddenError('Нет прав');
-//       }
-//       const deletedMovie = movie;
-//       movie.remove();
-//       res.send(deletedMovie);
-//     })
-//     .catch((err) => next(err));
-// };
 
 const deleteMovie = (req, res, next) => {
   Movie.findOne(
